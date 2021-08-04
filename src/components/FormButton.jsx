104727d55a56ab3c4580ca64e5d8081361d8ba0f
muzaffarmhm <muzaffarmhm@gmail.com>
2021-08-04 23:54:48 +0530

    Added Button for Write Page

--- src/components/FormButton.jsx
+++ src/components/FormButton.jsx
@@ -8,6 +8,7 @@ export default function FormButton(props) {
         defaultValue="Sign up with email"
         className="py-3 bg-green-400 text-white w-full rounded hover:bg-green-600 cursor-pointer"
         onClick={props.onClick}
+        value={props.value}
       />
     </div>
   );
